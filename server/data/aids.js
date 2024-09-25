bbe8460c88acfda98b996854f370dee875a7adcf
jawadrada <jawadrada18@gmail.com>
2024-09-25 12:22:21 -0400

    Added real data

--- server/data/aids.js
+++ server/data/aids.js
@@ -1,53 +1,53 @@
 const aids = [
     {
         "id": 1,
-        "name": "Helping Hands",
-        "description": "A non-profit organization dedicated to providing food and shelter to the homeless.",
-        "donationLink": "https://www.helpinghands.org/donate",
-        "volunteerLink": "https://www.helpinghands.org/volunteer",
-        "image": "https://www.helpinghands.org/images/logo.png",
-        "submittedBy": "John Doe",
-        "submittedOn": "2023-10-01"
+        "name": "Doctors Without Borders",
+        "description": "In the face of overwhelming emergencies, Doctors Without Borders is a lifeline for people in more than 70 countries around the world. Whether we’re delivering surgical care in conflict zones, treating children for malnutrition, or offering mental health care in the aftermath of crises.", 
+        "donationLink": "https://donate.doctorswithoutborders.org/secure/dont-turn-away-triple-match-your-gift-and-save-lives-sdv?ms=ADD2310U1U80&utm_source=bing&utm_medium=cpc&utm_campaign=NONBRAND_CKMSF-NONBRAND-BS-BS-ALL-GazaRR.Exact-BO-ALL-RSA-GazaSupportRR23-ONETIME&msclkid=05b405af712c1ec2b7cb635c57a0b117",
+        "volunteerLink": "https://www.doctorswithoutborders.org/get-involved/volunteer",
+        "image": "https://th.bing.com/th/id/OIP.LlSdJ3KV_KCUV_v1tS0IYwHaHa?rs=1&pid=ImgDetMain",
+        "submittedBy": "Jawad Rada",
+        "submittedOn": "2024-09-24"
       },
       {
         "id": 2,
-        "name": "Global Aid Network",
-        "description": "An international organization focused on disaster relief and humanitarian aid.",
-        "donationLink": "https://www.globalaidnetwork.org/donate",
-        "volunteerLink": "https://www.globalaidnetwork.org/volunteer",
-        "image": "https://www.globalaidnetwork.org/images/logo.png",
-        "submittedBy": "Jane Smith",
-        "submittedOn": "2023-10-02"
+        "name": "Save the Children",
+        "description": "Save the Children is the global leader in child-focused humanitarian response. We’ve been a lifeline for families fleeing violence in Syria, Venezuela, Myanmar and dozens of other crises around the world.",
+        "donationLink": "https://www.savethechildren.org/us/where-we-work/west-bank-gaza#:~:text=DONATE%20NOW.%20Save%20the%20Children's%20Humanitarian",
+        "volunteerLink": "https://www.savethechildren.org/us/ways-to-help/how-to-volunteer#:~:text=Learn%20about%20how%20to%20volunteer%20with",
+        "image": "https://th.bing.com/th/id/R.ffee24e58cc25535401ccbca501d7b34?rik=nztvgoOilqbaRA&pid=ImgRaw&r=0",
+        "submittedBy": "Jawad Rada",
+        "submittedOn": "2024-09-24"
       },
       {
         "id": 3,
-        "name": "Global Aid Network",
-        "description": "An international organization focused on disaster relief and humanitarian aid.",
-        "donationLink": "https://www.globalaidnetwork.org/donate",
-        "volunteerLink": "https://www.globalaidnetwork.org/volunteer",
-        "image": "https://www.globalaidnetwork.org/images/logo.png",
-        "submittedBy": "Jane Smith",
-        "submittedOn": "2023-10-02"
+        "name": "UNRWA",
+        "description": "The United Nations Relief and Works Agency for Palestine Refugees in the Near East (UNRWA) is a relief and human development agency. UNRWA is funded almost entirely by voluntary contributions from UN Member States.",
+        "donationLink": "https://donate.unrwa.org/-landing-page/en_EN#:~:text=UNRWA%20human%20development%20and%20humanitarian%20services",
+        "volunteerLink": "https://www.unrwausa.org/work-with-us/#:~:text=Volunteer.%20Turn%20your%20solidarity%20with%20Palestine",
+        "image": "https://th.bing.com/th/id/OIP.0Y0_fotQ7mVPgt3mwjc0lQAAAA?rs=1&pid=ImgDetMain",
+        "submittedBy": "Jawad Rada",
+        "submittedOn": "2024-09-24"
       },
       {
         "id": 4,
-        "name": "Global Aid Network",
-        "description": "An international organization focused on disaster relief and humanitarian aid.",
-        "donationLink": "https://www.globalaidnetwork.org/donate",
-        "volunteerLink": "https://www.globalaidnetwork.org/volunteer",
-        "image": "https://www.globalaidnetwork.org/images/logo.png",
-        "submittedBy": "Jane Smith",
-        "submittedOn": "2023-10-02"
+        "name": "SALAM",
+        "description": "Salam LADC was founded by Joseph Matta in South Lebanon as a response to the 2006 Lebanon War, and registered with the Ministry of the Interior as a non-profit association in 2009. Salam LADC’s primary aim is to strengthen dialogue and cooperation between the different communities living in Lebanon. Salam LADC focuses on sustainable projects that provide tools and expertise to local communities; youth, emergency response and environmental teams have been created and trained in municipalities across the country.",
+        "donationLink": "https://salamladc.org/donate/",
+        "volunteerLink": "https://salamladc.org/volunteer/",
+        "image": "https://th.bing.com/th/id/OIP.uBVhn6AeEVIPpk_ON3SF1AHaHa?rs=1&pid=ImgDetMain",
+        "submittedBy": "Jawad Rada",
+        "submittedOn": "2024-09-24"
       },
       {
         "id": 5,
-        "name": "Medical Relief Foundation",
-        "description": "Providing medical assistance and supplies to underprivileged communities worldwide.",
-        "donationLink": "https://www.medicalrelieffoundation.org/donate",
-        "volunteerLink": "https://www.medicalrelieffoundation.org/volunteer",
-        "image": "https://www.medicalrelieffoundation.org/images/logo.png",
-        "submittedBy": "Alice Johnson",
-        "submittedOn": "2023-10-03"
+        "name": "Palestine Children's Relief Fund",
+        "description": "Palestine Children's Relief Fund (PCRF), founded in 1991 by concerned humanitarians in the USA, provides free medical care to thousands of injured and ill children yearly who lack local access to care within the local health care system.  Over the years, we've sent over 2,000 affected children abroad for free medical care, sent thousands of international doctors and nurses to provide tens of thousands of children free medical care in local hospitals, and provided tens of thousands of children humanitarian aid and support they otherwise would not get.",
+        "donationLink": "https://www.pcrf.net/#:~:text=Help%20Provide%20Urgent%20Humanitarian%20Care%20for",
+        "volunteerLink": "https://www.pcrf.net/get-involved",
+        "image": "https://th.bing.com/th/id/OIP.J40-wnXviAfiH9ZgsWArywHaEL?rs=1&pid=ImgDetMain",
+        "submittedBy": "Jawad Rada",
+        "submittedOn": "2024-09-24"
       }
     ]
 
