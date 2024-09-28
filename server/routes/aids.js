cbca1d913e8427d8d5b08496b6c7cc66a59091e7
jawadrada <jawadrada18@gmail.com>
2024-09-28 17:18:22 -0400

    Updated route aids.js

--- server/routes/aids.js
+++ server/routes/aids.js
@@ -1,20 +1,17 @@
-import express from 'express'
-import path from 'path'
-import { fileURLToPath } from 'url'
-import aidData from '../data/aids.js'
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import AidsController from '../controllers/aids.js';
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', (req, res) => {
-    console.log("Received request for /aids");
-    res.status(200).json(aidData);
-});
+router.get('/', AidsController.getAids);
 
 router.get('/:aidId', (req, res) => {
-    res.status(200).sendFile(path.resolve(__dirname, '../public/aid.html'))
-})
+    res.status(200).sendFile(path.resolve(__dirname, '../public/aid.html'));
+});
 
-export default router
+export default router;
