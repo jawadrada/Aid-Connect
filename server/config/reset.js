e7c232a2c56ce7b38ca3f5a8803a1d125be9e6d3
jawadrada <jawadrada18@gmail.com>
2024-09-28 17:12:23 -0400

    Added config directory

---
+++ server/config/reset.js
@@ -0,0 +1,59 @@
+import { pool } from './database.js';
+import './dotenv.js'
+import aidsData from '../data/aids.js';
+
+async function createAidsTable() {
+    const createTableQuery = `
+    DROP TABLE IF EXISTS aids;
+
+    CREATE TABLE IF NOT EXISTS aids (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        image TEXT NOT NULL,
+        donationLink TEXT NOT NULL,
+        volunteerLink TEXT NOT NULL,
+        description TEXT NOT NULL,
+        submittedBy VARCHAR(255) NOT NULL,
+        submittedOn TIMESTAMP NOT NULL
+    )
+    `
+
+    try {
+        const res = await pool.query(createTableQuery);
+        console.log('🎉 aids table created successfully');
+    } catch (err) {
+        console.error('⚠️ error creating aids table', err);
+    }
+}
+
+createAidsTable()
+
+const seedAidsTable = async () => {
+    await createAidsTable();
+
+    for (const aid of aidsData) {
+        const insertQuery = `
+        INSERT INTO aids (name, image, donationLink, volunteerLink, description, submittedBy, submittedOn)
+        VALUES ($1, $2, $3, $4, $5, $6, $7)
+        `;
+
+        const values = [
+            aid.name,
+            aid.image,
+            aid.donationLink,
+            aid.volunteerLink,
+            aid.description,
+            aid.submittedBy,
+            aid.submittedOn
+        ];
+
+        try {
+            await pool.query(insertQuery, values);
+            console.log(`✅ ${aid.name} added successfully`);
+        } catch (err) {
+            console.error('⚠️ error inserting aid', err);
+        }
+    }
+};
+
+seedAidsTable();
